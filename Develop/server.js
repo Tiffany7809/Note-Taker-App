6bbabaf22539e61df2e2fcb76b26b3fbfaaa160d
Tiffany Dempsey <tiffany.dempsey2000@gmail.com>
2022-05-04 00:23:44 -0700

    adding data persisttence so notes save

--- Develop/server.js
+++ Develop/server.js
@@ -48,15 +48,27 @@ if (title && text ) {
       review_id: uuid(),
     };
 
-    const noteString = JSON.stringify(newNote);
 
-    fs.writeFile(`./db/db.json`, noteString, (err) =>
-      err
-        ? console.error(err)
-        : console.log(
-            `Review for ${newNote.title} has been written to JSON file`
-          )
-    );
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+
+          // Convert string into JSON object
+          const parsedNote = JSON.parse(data);
+
+          parsedNote.push(newNote);
+
+          fs.writeFile(
+            './db/db.json',
+            JSON.stringify(parsedNote, null, 4),
+            (writeErr) =>
+              writeErr
+                ? console.error(writeErr)
+                : console.info('Successfully added a new note!')
+          );
+        }
+    });
 
     const response = {
       status: 'success',
