e6b48d923dddf3978a7f538d7e24ad91900dc867
Tiffany Dempsey <tiffany.dempsey2000@gmail.com>
2022-05-07 01:17:23 -0700

    adding route for new notes

--- Develop/server.js
+++ Develop/server.js
@@ -5,88 +5,76 @@ const fs = require('fs');
 
 const uuid = require('../helpers/uuid');
 
-
-
 const app = express();
-
+////////////////////////////////////////////////////////////////
+//assigning port number to listen on 
 const PORT = 3001;
-
-//host static files
+//////////////////////////////////////////////////////////////
+//middle ware
 app.use(express.static("public"));
-
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
+//////////////////////////////////////////////////////////////
+//get route for index.html file
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// make notes.html show when button is clicked
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/notes.html'))
-);
-
-//make stylesheet work properly
-
-
-
+///////////////////////////////////////////////////////////
 // read the db.json file
 app.get ('/api/notes', (req, res) => 
     res.json(note)
 ),
 
-//appends a new not to db.json
-app.post ('/api/notes', (req, res) => {
+/////////////////////////////////////////////////////////
+//get route for notes.html 
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
 
-const { title, text} = req.body;
+///////////////////////////////////////////////////////
+//Post route for new notes
+app.post("/api/notes", function (req, res) {
+  // Save new note to the request body, add it to the db.json file
+  const { title, text} = req.body;
 
-if (title && text ) {
-    // Variable for the Note object to be saved
-    const newNote = {
-      title,
-      text,
-      review_id: uuid(),
-    };
+  if (title && text ) {
+      // Variable for the Note object to be saved
+      const newNote = {
+        title,
+        text,
+        review_id: uuid(),
+      };
+   note.push(newNote);
+      }
+  // Return the new note
+  fs.writeFile("./db/db.json", JSON.stringify(note, null, 4), function () {
+    res.json(note);
+  });
+});
 
-
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        if (err) {
-          console.error(err);
-        } else {
-
-          // Convert string into JSON object
-          const parsedNote = JSON.parse(data);
-
-          parsedNote.push(newNote);
-
-          fs.writeFile(
-            './db/db.json',
-            JSON.stringify(parsedNote, null, 4),
-            (writeErr) =>
-              writeErr
-                ? console.error(writeErr)
-                : console.info('Successfully added a new note!')
-          );
-        }
-    });
-
-    const response = {
-      status: 'success',
-      body: newNote,
-    };
-
-    console.log(response);
-    res.status(201).json(response);
-  } else {
-    res.status(500).json('Error in posting note');
-  }
+//////////////////////////////////////////////////////
+//view previous notes when clicked
 
 
-})
 
-//make notes save to side menu when complete
+//////////////////////////////////////////////////////
+//delete funtion
 
-//Listening for requests
+
+////////////////////////////////////////////////
+ // If route is not found, re-route to home page
+ app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+
+
+//Listening for requests on port
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
+
+module.exports = app;
